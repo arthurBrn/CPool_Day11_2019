a6579468fd58d41e605a48f582440668c185cbd3
Baron <arthur.baron@epitech.eu>
2019-10-15 08:34:10 +0200

    15/10|08:33|Task 02 drafted. The code should work though no tests have be run on it.

---
+++ my_list_size.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** my_list_size
+** File description:
+** Day 11 Task 02
+*/
+
+#include "include/my."
+#include "incldue/mylist.h"
+
+int my_list_size(linked_list_t const *begin)
+{
+    linked_list_t const *current_list = begin;
+    int count = 0;
+
+    while (current_list != 0) {
+        count++;
+        current_list = current_list->next;
+    }
+    return (count);
+}
