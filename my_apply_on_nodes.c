c35eebb3a6095a4b4368e6474597d5041f6ba3bb
Baron <arthur.baron@epitech.eu>
2019-10-15 14:47:52 +0200

    15/10|14:47|Task 04 corrected. No need to create a f function when its given.

--- my_apply_on_nodes.c
+++ my_apply_on_nodes.c
@@ -9,15 +9,9 @@
 #include "include/mylist.h"
 #include <stdlib.h>
 
-void print_node_data(void *data)
-{
-    my_putstr(data);
-}
-
 int my_apply_on_nodes(linked_list_t *begin, int (*f)(void *))
 {
-    f = &print_node_data;
     while (begin->next != NULL)
-        f(begin->data);
+        (*f)(begin->data);
     return (0);
 }
