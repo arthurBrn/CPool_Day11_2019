8c771f026c53f0dfae73743416328101c9170706
Baron <arthur.baron@epitech.eu>
2019-10-15 15:40:22 +0200

    15/10|15:40|Task 06 done. my_find_node.c

---
+++ my_find_node.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** my_find_node
+** File description:
+** Day 11 Task 06
+*/
+
+#include "include/my.h"
+#include "include/mylist.h"
+#include <stdlib.h>
+
+linked_list_t *my_find_node(linked_list_t const *begin, void const *data_ref, int (*cmp)())
+{
+    while (begin != NULL) {
+        if ((*cmp)(begin->data, date_ref))
+            return begin;
+        else
+            return (0);
+        begin = begin->next;
+    }
+}
