3af37753cea41658a6f772a9c71d6bd637f30d0d
Baron <arthur.baron@epitech.eu>
2019-10-15 09:55:03 +0200

    15/10|09:54|my_list_size.c comparing ->next value to null instead of 0

--- my_list_size.c
+++ my_list_size.c
@@ -7,13 +7,14 @@
 
 #include "include/my.h"
 #include "incldue/mylist.h"
+#include <stdlib.h>
 
 int my_list_size(linked_list_t const *begin)
 {
     linked_list_t const *current_list = begin;
     int count = 0;
 
-    while (current_list != 0) {
+    while (current_list != NULL) {
         count++;
         current_list = current_list->next;
     }
