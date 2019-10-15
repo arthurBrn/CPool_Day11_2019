a885207ccbe48d0485b80d3002704fdf953a204b
Baron <arthur.baron@epitech.eu>
2019-10-15 13:52:54 +0200

    15/10|13:52|Task 03 done. my_rev_list.c

---
+++ my_rev_list.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2019
+** CPool_Day11_2019
+** File description:
+** my_rev_list.c
+*/
+
+#include <stdlib.h>
+#include "include/my.h"
+#include "include/mylist.h"
+int my_list_size(linked_list_t const *begin);
+
+void my_rev_list(linked_list_t **begin)
+{
+    int list_size = my_list_size(begin);
+    int j = 0;
+    linked_list_t *prev = NULL;
+    linked_list_t *cur = begin;
+    linked_list_t *next = NULL;
+
+    while (list_size > j) {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+        j++;
+    }
+}
