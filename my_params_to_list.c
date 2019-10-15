6a32cddf80f1081602f8019b502c5e6562a0142f
Baron <arthur.baron@epitech.eu>
2019-10-15 22:04:20 +0200

    15/10|22:04|Task 01 corrected.

--- my_params_to_list.c
+++ my_params_to_list.c
@@ -11,19 +11,18 @@
 
 linked_list_t *my_params_to_list(int ac, char * const *av)
 {
-    int i = ac;
-    linked_list_t * head;
-    linked_list_t * start;
+    int i = 0;
+    linked_list_t *element = NULL;
+    linked_list_t *tempo = NULL;
 
-    head = malloc(sizeof(linked_list_t));
-    start = head;
     if (ac < 1)
         return (0);
     while (ac > i) {
-        head->data = av[i];
-        head->next = malloc(sizeof(linked_list_t));
-        head = head->next;
-        i--;
+        element = malloc(sizeof(linked_list_t));
+        element->data = av[i];
+        element->tempo;
+        tempo = element;
+        i++;
     }
-    return (start);
+    return (element);
 }
